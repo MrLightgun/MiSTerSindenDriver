486a8ec21a0aaf9d018be57871460fb840880b4a
MrLightgun <40304251+MrLightgun@users.noreply.github.com>
2023-11-20 11:58:18 +0000

    Add files via upload

---
+++ install_MOnSieur.sh
@@ -0,0 +1,8 @@
+#Download Sinden files on MOnSieurFPGA Feb 2022 release
+sudo mkdir /home/alarm/Lightgun
+cd /home/alarm/Lightgun
+sudo wget https://github.com/MrLightgun/MiSTerSindenDriver/archive/refs/heads/main.zip
+sudo unzip main.zip
+cd /home/alarm/Lightgun/MiSTerSindenDriver-main
+sudo chmod +x *.sh
+./install.sh
---
+++ install_MrFusion.sh
@@ -0,0 +1,7 @@
+#Download Sinden files on MiSTer using MrFusion 2.7
+sudo mkdir /media/fat/Lightgun
+cd /media/fat/Lightgun
+sudo wget https://github.com/MrLightgun/MiSTerSindenDriver/archive/refs/heads/main.zip
+sudo unzip main.zip
+cd /media/fat/Lightgun/MiSTerSindenDriver-main
+./install2.sh
