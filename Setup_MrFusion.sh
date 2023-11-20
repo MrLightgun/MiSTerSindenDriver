aa927de94d353ab2142a4c6f4745b146ffec49f0
MrLightgun <40304251+MrLightgun@users.noreply.github.com>
2023-11-20 12:26:29 +0000

    Update Setup_MrFusion.sh

--- Setup_MrFusion.sh
+++ Setup_MrFusion.sh
@@ -11,8 +11,12 @@ sudo cp *.sh /media/fat/scripts
 sudo chmod +x /media/fat/scripts/*.sh
 
 #copy lightgun driver
-cd /media/fat/Lightgun/MiSTerSindenDriver-main/StartupScripts
-sudo cp *.* /media/fat/Lightgun
+cd /media/fat/Lightgun/MiSTerSindenDriver-main/LightgunDriver
+sudo cp *.* /media/fat/Lightgun/
+mkdir /media/fat/usr/lib
+ln -s /media/fat/usr/lib /usr/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/media/fat/Lightgun
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
 
 #copy tweaked kernel
 cd /media/fat/Lightgun/MiSTerSindenDriver-main/Kernel/MOnSieurFPGA/28thFeb2022/
