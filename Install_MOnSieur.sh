1348ba3a6dfc8e3dc83a21f84d37b820b9eec20a
MrLightgun <40304251+MrLightgun@users.noreply.github.com>
2024-06-27 12:16:07 +0100

    Delete Install_MOnSieur.sh

--- Install_MOnSieur.sh
+++
@@ -1,8 +0,0 @@
-#Download Sinden files on MOnSieurFPGA Feb 2022 release
-sudo mkdir /home/alarm/Lightgun
-cd /home/alarm/Lightgun
-sudo wget https://github.com/MrLightgun/MiSTerSindenDriver/archive/refs/heads/main.zip
-sudo unzip main.zip
-cd /home/alarm/Lightgun/MiSTerSindenDriver-main
-sudo chmod +x *.sh
-./Setup_MrFusion.sh
