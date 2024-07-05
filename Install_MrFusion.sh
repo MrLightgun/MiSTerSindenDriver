28e8312a2548992025920769cdd145c9861c29ed
MrLightgun <40304251+MrLightgun@users.noreply.github.com>
2024-07-05 20:52:40 +0100

    Update Install_MrFusion.sh

--- Install_MrFusion.sh
+++ Install_MrFusion.sh
@@ -2,6 +2,6 @@
 sudo mkdir /media/fat/Lightgun
 cd /media/fat/Lightgun
 sudo wget https://github.com/MrLightgun/MiSTerSindenDriver/archive/refs/heads/main.zip
-sudo unzip main.zip
+sudo unzip -a main.zip
 cd /media/fat/Lightgun/MiSTerSindenDriver-main
 ./Setup_MrFusion.sh
