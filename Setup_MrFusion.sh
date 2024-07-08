bfd0de4e733b399c4118537f0d7bfecc42883b62
Matt-Retrogamer <matt.retrogamer@gmail.com>
2024-07-08 16:47:18 +0200

    Further improved the install, setup and uninstall scripts

--- Setup_MrFusion.sh
+++ Setup_MrFusion.sh
@@ -1,42 +1,46 @@
+#!/bin/bash
 
-#copy custom cores that have Sinden border built in
+# Copy custom cores that have Sinden border built in
 cd /media/fat/Lightgun/MiSTerSindenDriver-main/Cores
-sudo mkdir /media/fat/_Console/
-sudo cp *.rbf /media/fat/_Console/
+sudo mkdir -p /media/fat/_Console/
+sudo cp -f *.rbf /media/fat/_Console/
 sudo chmod 755 /media/fat/_Console/*.rbf
 
-#copy startup scripts to easily start and stop the lightgun
+# Copy startup scripts to easily start and stop the lightgun
 cd /media/fat/Lightgun/MiSTerSindenDriver-main/StartupScripts/MrFusion
-sudo cp *.sh /media/fat/scripts
+sudo cp -f *.sh /media/fat/scripts
 sudo chmod +x /media/fat/scripts/*.sh
 
-#copy lightgun driver
+# Copy lightgun driver
 cd /media/fat/Lightgun/MiSTerSindenDriver-main/LightgunDriver
-sudo cp *.* /media/fat/Lightgun/
-sudo cp LightgunDriver /media/fat/Lightgun/LightgunDriver
+sudo cp -f *.* /media/fat/Lightgun/
+sudo cp -f LightgunDriver /media/fat/Lightgun/LightgunDriver
 cd /media/fat
-sudo mkdir usr
+sudo mkdir -p usr
 cd usr
-sudo mkdir lib
-sudo cp /media/fat/Lightgun/lib* /usr/lib
+sudo mkdir -p lib
+sudo cp -f /media/fat/Lightgun/lib* /usr/lib
 
-#copy tweaked kernel
+# Copy tweaked kernel
 cd /media/fat/Lightgun/MiSTerSindenDriver-main/Kernel/MiSTerFPGA/MrFusion2.7/
-sudo cp /media/fat/linux/zImage_dtb /media/fat/linux/zImage_dtb_backup
-sudo cp zImage_dtb /media/fat/linux/zImage_dtb
+sudo cp -f /media/fat/linux/zImage_dtb /media/fat/linux/zImage_dtb_backup
+sudo cp -f zImage_dtb /media/fat/linux/zImage_dtb
 
-#now update MiSTer to be able to read the Sinden lightgun joystick
+# Now update MiSTer to be able to read the Sinden lightgun joystick
 cd /media/fat/Lightgun/MiSTerSindenDriver-main/MiSTerUpdate
-sudo cp /media/fat/MiSTer /media/fat/MiSTer_orig
-#may need to close MiSTer on the front end
-kill -9 $(pidof MiSTer)
+sudo cp -f /media/fat/MiSTer /media/fat/MiSTer_orig
 
-sudo cp MiSTer /media/fat/MiSTer
+# Check if MiSTer is running and stop it if necessary
+if [[ ! -z $(pidof MiSTer) ]]; then
+    sudo kill -9 $(pidof MiSTer)
+fi
+
+sudo cp -f MiSTer /media/fat/MiSTer
 sudo chmod u+x /media/fat/MiSTer
 
-#Copy over configs to setup lightgun as joystick correctly on MiSTer menu and in cores
-sudo mkdir /media/fat/config/inputs
-sudo cp /media/fat/Lightgun/MiSTerSindenDriver-main/Config/*.* /media/fat/config
-sudo cp /media/fat/Lightgun/MiSTerSindenDriver-main/Config/inputs/*.* /media/fat/config/inputs
+# Copy over configs to setup lightgun as joystick correctly on MiSTer menu and in cores
+sudo mkdir -p /media/fat/config/inputs
+sudo cp -f /media/fat/Lightgun/MiSTerSindenDriver-main/Config/*.* /media/fat/config
+sudo cp -f /media/fat/Lightgun/MiSTerSindenDriver-main/Config/inputs/*.* /media/fat/config/inputs
 
-
+echo "MiSTerSindenDriver setup completed. Please reboot your MiSTer."
