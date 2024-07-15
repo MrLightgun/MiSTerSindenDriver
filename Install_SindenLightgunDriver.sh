5b575d73d92ebe227d9cdc124ad098274db51307
Matt-Retrogamer <matt.retrogamer@gmail.com>
2024-07-15 22:18:06 +0200

    Revert to Matt-Retrogamer repo in the install script

--- Install_SindenLightgunDriver.sh
+++ Install_SindenLightgunDriver.sh
@@ -13,7 +13,7 @@ sudo mkdir -p /media/fat/Scripts/Sinden
 cd /media/fat/Scripts/Sinden
 
 # Download Sinden files on MiSTer using MrFusion 2.7
-sudo wget -O main.zip https://github.com/MrLightgun/MiSTerSindenDriver/archive/refs/heads/main.zip
+sudo wget -O main.zip https://github.com/Matt-Retrogamer/MiSTerSindenDriver/archive/refs/heads/main.zip
 sudo unzip -o main.zip
 
 # Navigate to the downloaded directory
