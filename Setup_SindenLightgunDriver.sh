9679a28910acac0bc9f84aa5416944eb4666cb2c
terminator2k2 <48845336+terminator2k2@users.noreply.github.com>
2025-03-13 16:01:49 +0000

    update

--- Setup_SindenLightgunDriver.sh
+++ Setup_SindenLightgunDriver.sh
@@ -6,11 +6,16 @@ set -e
 # Define the path for Sinden scripts
 SINDEN_SCRIPTS_PATH="/media/fat/Scripts/Sinden"
 
-# Copy custom cores that have Sinden border built in
+# Copy custom cores and mgl files that have Sinden border built in
 cd $SINDEN_SCRIPTS_PATH/MiSTerSindenDriver-main/Cores
+mkdir -p /media/fat/Light Gun/
+cp -f *.rbf /media/fat/Light Gun/
+chmod 755 /media/fat/Light Gun/*.rbf
+
+cd $SINDEN_SCRIPTS_PATH/MiSTerSindenDriver-main/mgl
 mkdir -p /media/fat/_Console/
-cp -f *.rbf /media/fat/_Console/
-chmod 755 /media/fat/_Console/*.rbf
+cp -f *.mgl/media/fat/_Console/
+chmod 755 /media/fat/_Console/*.mgl
 
 # Copy startup scripts to easily start and stop the lightgun
 cd $SINDEN_SCRIPTS_PATH/MiSTerSindenDriver-main/StartupScripts
@@ -21,17 +26,6 @@ cd $SINDEN_SCRIPTS_PATH/MiSTerSindenDriver-main/Kernel/MiSTerFPGA/MrFusion2.7/
 cp -f /media/fat/linux/zImage_dtb /media/fat/linux/zImage_dtb_backup
 cp -f zImage_dtb /media/fat/linux/zImage_dtb
 
-# Now update MiSTer to be able to read the Sinden lightgun joystick
-cd $SINDEN_SCRIPTS_PATH/MiSTerSindenDriver-main/MiSTerUpdate
-cp -f /media/fat/MiSTer /media/fat/MiSTer_orig
-
-# Check if MiSTer is running and stop it if necessary
-if [[ ! -z $(pidof MiSTer) ]]; then
-    kill -9 $(pidof MiSTer)
-fi
-
-cp -f MiSTer /media/fat/MiSTer
-chmod u+x /media/fat/MiSTer
 
 # Copy over configs to setup lightgun as joystick correctly on MiSTer menu and in cores
 mkdir -p /media/fat/config/inputs
